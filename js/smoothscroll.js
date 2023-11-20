2ed17dcf9ac3bcc21e401c59785c7a00fd56dda3
matty6223 <S3879003@student.rmit.edu.au>
2023-11-20 22:22:27 +1000

    fixed issue with smooth scroll

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -24,19 +24,8 @@ $('a[href*="#"]')
         // Only prevent default if animation is actually gonna happen
         event.preventDefault();
         $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 500, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
+          scrollTop: target.offset().top - 100
+        }, 500, function() {});
       }
     }
   });
